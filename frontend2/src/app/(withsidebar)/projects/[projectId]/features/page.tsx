a55c651c028e0ea219f48038d8b30e477b5fa7f1
Aniket Gaikwad <aniket03g@gmail.com>
2025-06-04 00:08:37 -0700

    Tags delete working inline

--- frontend2/src/app/(withsidebar)/projects/[projectId]/features/page.tsx
+++ frontend2/src/app/(withsidebar)/projects/[projectId]/features/page.tsx
@@ -68,7 +68,7 @@ export default function FeaturesPage() {
           className="bg-blue-600 text-white px-4 py-2 rounded shadow hover:bg-blue-700 transition"
           onClick={() => setShowFeatureModal(true)}
         >
-          + Add Feature
+          + CREATE FEATURE
         </button>
       </div>
 
